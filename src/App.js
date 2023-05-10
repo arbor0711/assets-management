a6e5f3a5f1cc912451a57dd9fb20d8d4fea4fb2e
Alireza <arbor0711@gmail.com>
2023-05-10 22:55:02 +0200

    Audio aasset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+  const me = require("./assets/images/image.jpg");
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  }
+
+  function toggle2() {
+    bird2.paused ? bird2.play() : bird2.pause();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <img src={me} width="333px" alt="me logo" />
+        <br />
+        <button onClick={toggle1}>Bird one</button>
+        <button onClick={toggle2}>Bird two</button>
+      </div>
+    </>
   );
 }
 
